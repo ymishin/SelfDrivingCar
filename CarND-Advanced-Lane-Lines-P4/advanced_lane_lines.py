c3ba25fc2d78fe3b5ca3b80597af3c5abedf12dc
ymishin <yury.mishin@gmail.com>
2018-08-16 19:29:54 +0200

    Work on the project "Advanced Lane Finding".

--- CarND-Advanced-Lane-Lines-P4/advanced_lane_lines.py
+++ CarND-Advanced-Lane-Lines-P4/advanced_lane_lines.py
@@ -3,6 +3,7 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 import os
+from moviepy.editor import VideoFileClip
 
 ############################################################################################################
 
@@ -41,13 +42,15 @@ def calibrate():
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[1::-1], None, None)
     return mtx, dist
 
+mtx, dist = calibrate()
+
 ############################################################################################################
 
 # Threshold functions
 
 # identify pixels where x- or y-gradient falls within specified range
 def sobel_threshold(img, sobel_kernel=3, orient='x', thresh=(0, 255)):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     if (orient == 'x'):
         sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     elif (orient == 'y'):
@@ -60,7 +63,7 @@ def sobel_threshold(img, sobel_kernel=3, orient='x', thresh=(0, 255)):
 
 # identify pixels where total gradient falls within specified range
 def mag_threshold(img, sobel_kernel=3, thresh=(0, 255)):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     sobel = np.sqrt(sobelx**2 + sobely**2)
@@ -72,7 +75,7 @@ def mag_threshold(img, sobel_kernel=3, thresh=(0, 255)):
 
 # identify pixels where gradient direction falls within specified range
 def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     sobeldir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
@@ -82,15 +85,15 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
 
 # identify pixels where S-channel of HLS falls within specified range
 def s_threshold(img, thresh=(0, 255)):
-    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     s_channel = hls[:,:,2]
     output = np.zeros_like(s_channel)
     output[(s_channel >= thresh[0]) & (s_channel <= thresh[1])] = 1
     return output
 
-# identify pixels where R-channel of BGR falls within specified range
+# identify pixels where R-channel of RGB falls within specified range
 def r_threshold(img, thresh=(0, 255)):
-    r_channel = img[:,:,2]
+    r_channel = img[:,:,0]
     output = np.zeros_like(r_channel)
     output[(r_channel >= thresh[0]) & (r_channel <= thresh[1])] = 1
     return output
@@ -178,7 +181,7 @@ def find_lane_pixels(binary_warped):
 
     return leftx, lefty, rightx, righty, out_img
 
-def find_lanes_and_fit_polynomials(binary_warped):
+def find_lanes_and_fit_polynomials(binary_warped, output_mode=1):
 
     # Find our lane pixels first
     leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
@@ -198,17 +201,108 @@ def find_lanes_and_fit_polynomials(binary_warped):
         left_fitx = 1 * ploty ** 2 + 1 * ploty
         right_fitx = 1 * ploty ** 2 + 1 * ploty
 
-    ## Visualization ##
-    # Colors in the left and right lane regions
-    out_img[lefty, leftx] = [255, 0, 0]
-    out_img[righty, rightx] = [0, 0, 255]
+    if (output_mode == 1):
+        ## Visualization ##
+        # Colors in the left and right lane regions
+        out_img[lefty, leftx] = [255, 0, 0]
+        out_img[righty, rightx] = [0, 0, 255]
 
-    # Plots the left and right polynomials on the lane lines
-    plt.imshow(out_img)
-    plt.xlim(0, 1280)
-    plt.ylim(720, 0)
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
+        # Plots the left and right polynomials on the lane lines
+        plt.imshow(out_img)
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+    elif (output_mode == 2):
+        # Create an image to draw the lines on
+        warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+        out_img = np.dstack((warp_zero, warp_zero, warp_zero))
+
+        # Recast the x and y points into usable format for cv2.fillPoly()
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(out_img, np.int_([pts]), (0, 255, 0))
+
+    return out_img
+
+############################################################################################################
+
+def process_image(img):
+
+    img_undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    # create thresholded binary image
+    ksize = 3
+    gradx_mask = sobel_threshold(img_undist, orient='x', sobel_kernel=ksize, thresh=(40, 255))
+    grady_mask = sobel_threshold(img_undist, orient='y', sobel_kernel=ksize, thresh=(40, 255))
+    # mag_mask = mag_thresh(img_undist, sobel_kernel=ksize, thresh=(20, 150))
+    # dir_mask = dir_threshold(img_undist, sobel_kernel=ksize, thresh=(0.7, 1.3))
+    r_mask = r_threshold(img_undist, thresh=(230, 255))
+    s_mask = s_threshold(img_undist, thresh=(170, 255))
+
+    combined = np.zeros_like(gradx_mask)
+    combined[(s_mask == 1) | (r_mask == 1) | ((gradx_mask == 1) & (grady_mask == 1))] = 1
+
+    # gradxy_mask = np.zeros_like(gradx_mask)
+    # gradxy_mask[((gradx_mask == 1) & (grady_mask == 1))] = 1
+
+    # apply a perspective transform to rectify binary image ("birds-eye view")
+    src_coord = np.float32([[700, 450], [1110, 660], [190, 660], [580, 450]])
+    offset = 100
+    img_size = (img_undist.shape[1], img_undist.shape[0])
+    # offset = 0
+    # img_size = (300, 300)
+    dst_coord = np.float32([[img_size[0] - offset, offset], [img_size[0] - offset, img_size[1] - offset],
+                            [offset, img_size[1] - offset], [offset, offset]])
+    M = cv2.getPerspectiveTransform(src_coord, dst_coord)
+    # Minv = cv2.getPerspectiveTransform(dst_coord, src_coord)
+    combined = cv2.warpPerspective(combined, M, img_size, flags=cv2.INTER_LINEAR)
+
+    out_img = find_lanes_and_fit_polynomials(combined)
+    return out_img
+
+############################################################################################################
+
+def process_video_frame(img):
+
+    img_undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    # create thresholded binary image
+    ksize = 3
+    gradx_mask = sobel_threshold(img_undist, orient='x', sobel_kernel=ksize, thresh=(40, 255))
+    grady_mask = sobel_threshold(img_undist, orient='y', sobel_kernel=ksize, thresh=(40, 255))
+    # mag_mask = mag_thresh(img_undist, sobel_kernel=ksize, thresh=(20, 150))
+    # dir_mask = dir_threshold(img_undist, sobel_kernel=ksize, thresh=(0.7, 1.3))
+    r_mask = r_threshold(img_undist, thresh=(230, 255))
+    s_mask = s_threshold(img_undist, thresh=(170, 255))
+
+    combined = np.zeros_like(gradx_mask)
+    combined[(s_mask == 1) | (r_mask == 1) | ((gradx_mask == 1) & (grady_mask == 1))] = 1
+
+    # gradxy_mask = np.zeros_like(gradx_mask)
+    # gradxy_mask[((gradx_mask == 1) & (grady_mask == 1))] = 1
+
+    # apply a perspective transform to rectify binary image ("birds-eye view")
+    src_coord = np.float32([[700, 450], [1110, 660], [190, 660], [580, 450]])
+    offset = 100
+    img_size = (img_undist.shape[1], img_undist.shape[0])
+    # offset = 0
+    # img_size = (300, 300)
+    dst_coord = np.float32([[img_size[0] - offset, offset], [img_size[0] - offset, img_size[1] - offset],
+                            [offset, img_size[1] - offset], [offset, offset]])
+    M = cv2.getPerspectiveTransform(src_coord, dst_coord)
+    Minv = cv2.getPerspectiveTransform(dst_coord, src_coord)
+    combined = cv2.warpPerspective(combined, M, img_size, flags=cv2.INTER_LINEAR)
+
+    out_img = find_lanes_and_fit_polynomials(combined, 2)
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    out_img = cv2.warpPerspective(out_img, Minv, (img.shape[1], img.shape[0]))
+    # Combine the result with the original image
+    out_img = cv2.addWeighted(img_undist, 1, out_img, 0.3, 0)
 
     return out_img
 
@@ -217,79 +311,88 @@ def find_lanes_and_fit_polynomials(binary_warped):
 def process_images():
 
     images = glob.glob('./test_images/*.jpg')
-    # images = glob.glob('./camera_cal/calibration1.jpg')
-
-    mtx, dist = calibrate()
 
     for fname in images:
         # apply a distortion correction
         img = cv2.imread(fname)
-        img_undist = cv2.undistort(img, mtx, dist, None, mtx)
-
-        # create thresholded binary image
-        ksize = 3
-        gradx_mask = sobel_threshold(img_undist, orient='x', sobel_kernel=ksize, thresh=(40, 255))
-        grady_mask = sobel_threshold(img_undist, orient='y', sobel_kernel=ksize, thresh=(40, 255))
-        # mag_mask = mag_thresh(img_undist, sobel_kernel=ksize, thresh=(20, 150))
-        # dir_mask = dir_threshold(img_undist, sobel_kernel=ksize, thresh=(0.7, 1.3))
-        r_mask = r_threshold(img_undist, thresh=(230, 255))
-        s_mask = s_threshold(img_undist, thresh=(170, 255))
-
-        combined = np.zeros_like(gradx_mask)
-        combined[(s_mask == 1) | (r_mask == 1) | ((gradx_mask == 1) & (grady_mask == 1))] = 1
-
-        # gradxy_mask = np.zeros_like(gradx_mask)
-        # gradxy_mask[((gradx_mask == 1) & (grady_mask == 1))] = 1
-
-        # apply a perspective transform to rectify binary image ("birds-eye view")
-        src_coord = np.float32([[700, 450], [1110, 660], [190, 660], [580, 450]])
-        offset = 100
-        img_size = (img_undist.shape[1], img_undist.shape[0])
-        # offset = 0
-        # img_size = (300, 300)
-        dst_coord = np.float32([[img_size[0] - offset, offset], [img_size[0] - offset, img_size[1] - offset],
-                                [offset, img_size[1] - offset], [offset, offset]])
-        M = cv2.getPerspectiveTransform(src_coord, dst_coord)
-        # Minv = cv2.getPerspectiveTransform(dst_coord, src_coord)
-        combined = cv2.warpPerspective(combined, M, img_size, flags=cv2.INTER_LINEAR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         plt.figure()
-        out_img = find_lanes_and_fit_polynomials(combined)
-
-        # plt.imshow(img_undist)
-        # plt.plot(src_coord[0][0], src_coord[0][1], '.')
-        # plt.plot(src_coord[1][0], src_coord[1][1], '.')
-        # plt.plot(src_coord[2][0], src_coord[2][1], '.')
-        # plt.plot(src_coord[3][0], src_coord[3][1], '.')
-
-        # plt.imshow(s_mask, cmap='gray')
-        # plt.axis('off')
+        out_img = process_image(img)
         filename, file_extension = os.path.splitext(fname)
-        # plt.savefig(filename + '_smask' + '.jpeg')
-        # plt.imshow(r_mask, cmap='gray')
-        # plt.axis('off')
-        # plt.savefig(filename + '_rmask' + '.jpeg')
-        # plt.imshow(gradx_mask, cmap='gray')
-        # plt.axis('off')
-        # plt.savefig(filename + '_gradx' + '.jpeg')
-        # plt.imshow(grady_mask, cmap='gray')
-        # plt.axis('off')
-        # plt.savefig(filename + '_grady' + '.jpeg')
-        # plt.imshow(gradxy_mask, cmap='gray')
-        # plt.axis('off')
-        # plt.savefig(filename + '_gradxy' + '.jpeg')
-
-        # plt.imshow(combined, cmap='gray')
-        # plt.axis('off')
         plt.savefig(filename + '_out' + '.jpeg')
+
+        # img_undist = cv2.undistort(img, mtx, dist, None, mtx)
+        #
+        # # create thresholded binary image
+        # ksize = 3
+        # gradx_mask = sobel_threshold(img_undist, orient='x', sobel_kernel=ksize, thresh=(40, 255))
+        # grady_mask = sobel_threshold(img_undist, orient='y', sobel_kernel=ksize, thresh=(40, 255))
+        # # mag_mask = mag_thresh(img_undist, sobel_kernel=ksize, thresh=(20, 150))
+        # # dir_mask = dir_threshold(img_undist, sobel_kernel=ksize, thresh=(0.7, 1.3))
+        # r_mask = r_threshold(img_undist, thresh=(230, 255))
+        # s_mask = s_threshold(img_undist, thresh=(170, 255))
+        #
+        # combined = np.zeros_like(gradx_mask)
+        # combined[(s_mask == 1) | (r_mask == 1) | ((gradx_mask == 1) & (grady_mask == 1))] = 1
+        #
+        # # gradxy_mask = np.zeros_like(gradx_mask)
+        # # gradxy_mask[((gradx_mask == 1) & (grady_mask == 1))] = 1
+        #
+        # # apply a perspective transform to rectify binary image ("birds-eye view")
+        # src_coord = np.float32([[700, 450], [1110, 660], [190, 660], [580, 450]])
+        # offset = 100
+        # img_size = (img_undist.shape[1], img_undist.shape[0])
+        # # offset = 0
+        # # img_size = (300, 300)
+        # dst_coord = np.float32([[img_size[0] - offset, offset], [img_size[0] - offset, img_size[1] - offset],
+        #                         [offset, img_size[1] - offset], [offset, offset]])
+        # M = cv2.getPerspectiveTransform(src_coord, dst_coord)
+        # # Minv = cv2.getPerspectiveTransform(dst_coord, src_coord)
+        # combined = cv2.warpPerspective(combined, M, img_size, flags=cv2.INTER_LINEAR)
+        #
+        # plt.figure()
+        # out_img = find_lanes_and_fit_polynomials(combined)
+        #
+        # # plt.imshow(img_undist)
+        # # plt.plot(src_coord[0][0], src_coord[0][1], '.')
+        # # plt.plot(src_coord[1][0], src_coord[1][1], '.')
+        # # plt.plot(src_coord[2][0], src_coord[2][1], '.')
+        # # plt.plot(src_coord[3][0], src_coord[3][1], '.')
+        #
+        # # plt.imshow(s_mask, cmap='gray')
+        # # plt.axis('off')
+        # filename, file_extension = os.path.splitext(fname)
+        # # plt.savefig(filename + '_smask' + '.jpeg')
+        # # plt.imshow(r_mask, cmap='gray')
+        # # plt.axis('off')
+        # # plt.savefig(filename + '_rmask' + '.jpeg')
+        # # plt.imshow(gradx_mask, cmap='gray')
+        # # plt.axis('off')
+        # # plt.savefig(filename + '_gradx' + '.jpeg')
+        # # plt.imshow(grady_mask, cmap='gray')
+        # # plt.axis('off')
+        # # plt.savefig(filename + '_grady' + '.jpeg')
+        # # plt.imshow(gradxy_mask, cmap='gray')
+        # # plt.axis('off')
+        # # plt.savefig(filename + '_gradxy' + '.jpeg')
+        #
+        # # plt.imshow(combined, cmap='gray')
+        # # plt.axis('off')
+        # plt.savefig(filename + '_out' + '.jpeg')
 
 ############################################################################################################
 
 def process_video():
 
-    mtx, dist = calibrate()
+    input_clip_file = "project_video.mp4"
+    output_clip_file = "project_video_with_lanes.mp4"
+
+    input_clip = VideoFileClip(input_clip_file)
+    output_clip = input_clip.fl_image(process_video_frame)
+    output_clip.write_videofile(output_clip_file, audio=False)
 
 ############################################################################################################
 
-process_images()
-#process_video()
+#process_images()
+process_video()
